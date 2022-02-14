9f3a496eca0b34def24d3c5938c051e0fc2b7514
LiliF22 <lilian220198@gmail.com>
2022-02-13 18:05:50 -0600

    agregando modals a productos

--- popup.php
+++ popup.php
@@ -2,13 +2,13 @@
 <div class="mfp-content"><div class="ajax-text-and-image white-popup-block">
 	<style>
 	.ajax-text-and-image {
-		max-width:800px; margin: 20px auto; background: #FFF; padding: 0; line-height: 0;
+		max-width:800px; margin: 20px auto; background: #FFF; padding: 0; line-height: 0; border-radius: 30px;
 	}
 	.ajcol {
 		width: 50%; float:left;
 	}
 	.ajcol img {
-		width: 100%; height: auto;
+		width: 100%; height: auto; border-radius: 30px 0px 0px 30px;
 	}
 	@media all and (max-width:30em) {
 		.ajcol { 
@@ -27,7 +27,9 @@ $img= "";
 				switch ($_GET['id']) {
 					case 1:
 						$texto = "
-						<h3 class='nombre'>Carne de res</h3>
+						<h3 class='nombre'>CARNE DE RES</h3>
+						<span class='linea-ipad'></span>
+						<div class='text-modal'>
 						Rib Eye Wagyu<br>
 						Rib Eye de Res<br>
 						Aguja Norteña <br>
@@ -40,15 +42,87 @@ $img= "";
 						Short Rib <br>
 						Top Sirloin <br>
 						Bistec de Res <br>
-						Medallón de Res <br>";
+						Medallón de Res <br>
+						</div>";
 						$img= "img/carne_de_res.jpg";
 						break;
 					case 2:
-						echo "i es igual a 1";
+						$texto = "
+						<h3 class='nombre'>CARNE DE CERDO</h3>
+						<span class='linea-ipad'></span>
+						<div class='text-modal'>
+						Chicharrón de Cerdo<br>
+						Chuleta Ahumada de Cerdo<br>
+						Baby Back Ribs de Cerdo <br>
+						Pork Wings <br>
+						Pulled Pork<br>
+						Salchiribs <br>
+						Carnitas de Cerdo <br>
+						Asado de Cerdo <br>
+						Tinga de Cerdo <br>
+						</div>";
+						$img="img/carne_de_cerdo.jpg";
 						break;
 					case 3:
-						echo "i es igual a 2";
+						$texto = "
+						<h3 class='nombre'>CARNE DE POLLO</h3>
+						<span class='linea-ipad'></span>
+						<div class='text-modal'>
+						Pechuga de Pollo sin Piel<br>
+						Pechuga Cocida Deshebrada<br>
+						Piernas <br>
+						Muslos <br>
+						Pierna y Muslo <br>
+						Milanesas <br>
+						Milanesas Empanizadas <br>
+						Fajitas <br>
+						Fajitas Empanizadas <br>
+						Tenders Empanizados <br>
+						Boneless <br>
+						Nuggets <br>
+						Palomitas de Pechuga de Pollo <br>
+						</div>";
+						$img="img/carne_de_pollo.jpg";
 						break;
+						case 4:
+							$texto = "
+							<h3 class='nombre'>PESCADOS Y MARISCOS</h3>
+							<span class='linea-ipad'></span>
+							<div class='text-modal'>
+							Salmón <br>
+							Camarón <br>
+							Medallones de Atún <br>
+							Tilapia <br>
+							Pulpo <br>
+							Basa <br>
+							Huachinango <br>
+							Callo de hacha <br>
+							Surimi <br>
+							Mejillones <br>
+							y más...
+							</div>";
+							$img="img/mariscos.jpg";
+							break;
+							case 5:
+								$texto = "
+								<h3 class='nombre'>OTROS</h3>
+								<span class='linea-ipad'></span>
+								<div class='text-modal'>
+								Carne para Hamburguesa <br>
+								La Ramos <br>
+								Chicharrón de la Ramos <br>
+								Papas a la Francesa <br>
+								Papas Gajo <br>
+								Aros de Cebolla <br>
+								Discada <br>
+								Chistorra <br>
+								Tortillas de Maíz <br>
+								Frutos Congelados <br>
+								Helados <br>
+								y más...
+								</div>";
+								$img="img/carne_de_cerdo.jpg";
+								break;
 				}
 			?>
 	<div class="ajcol">
